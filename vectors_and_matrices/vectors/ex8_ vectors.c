dc780a30ec4aabe51e07fabc4109e3b8c0ead838
Virgínia <virginiasoaresmuller@gmail.com>
2023-08-14 19:14:53 -0300

    exercicios

--- vectors_and_matrices/vectors/ex8_ vectors.c
+++ vectors_and_matrices/vectors/ex8_ vectors.c
@@ -1,3 +1,21 @@
 /*
 Create a program that reads 6 integer values and then displays the values read in reverse order on the screen.
 */
+
+#include <stdio.h>
+
+int main(){
+
+    int vector[6];
+
+    printf("Enter 6 numbers:\n");
+
+    for (int i=0; i < 6; i++){
+        scanf("%d",&vector[i]);
+    }
+
+    printf("reserve:");
+    for(int i = 5; i >= 0; i--){
+        printf("%d ", vector[i]);
+    }
+}
